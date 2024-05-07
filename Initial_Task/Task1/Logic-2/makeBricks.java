086bc09dbe23891ccc632920a597bc63335f342f
Vengat24 <vengatsrini2410@gmail.com>
2024-05-07 22:27:20 +0530

    Day 6 Task1

--- Initial_Task/Task1/Logic-2/makeBricks.java
+++
@@ -1,9 +0,0 @@
-public boolean makeBricks(int small, int big, int goal) {
-  if(goal%5==0 && goal/5 <= big)
-    return true;
-  else if (goal-big*5 <= small &&goal-big*5 >0 )
-    return true;
-  else if(goal%5 <= small && goal/5 <= big)
-    return true;
-  return false;
-}
